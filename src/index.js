f4fa165cbdbe7d9619a32d3e0c435368b807ebfa
NsanV <nsanv.dev@gmail.com>
2019-11-28 12:29:02 +0100

    it works

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./assets/style.css";
 import quizService from "./quizService";
-import QuestionBox from "./components/QuestionBox"
+import QuestionBox from "./components/QuestionBox";
+import Result from "./components/Result";
 
 class Quizz extends Component {
     state = {
-        questionBank: []
+        questionBank: [],
+        score: 0,
+        responses: 0
     };
     getQuestions = () => {
         quizService().then(question => {
@@ -14,7 +17,24 @@ class Quizz extends Component {
                 questionBank: question
             });
         });
-    } 
+    };
+    computeAnswer = (answer, correctAnswer) => {
+        if (answer === correctAnswer) {
+            this.setState({
+                score: this.state.score + 1
+            });
+        }
+        this.setState({
+            responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+        });
+    };
+    playAgain = () => {
+        this.getQuestions();
+        this.setState({
+            score: 0,
+            responses: 0
+        })
+    };
     componentDidMount() {
         this.getQuestions();
     }
@@ -22,9 +42,20 @@ class Quizz extends Component {
         return (
             <div className="container">
                 <div className="title">Quizz</div>
-                {this.state.questionBank.length > 0 && this.state.questionBank.map(({question, answers, correct, questionId}) => (
-                    <QuestionBox question={question} options={answers} key={questionId} />
-                ))}
+                { this.state.questionBank.length > 0 &&
+                    this.state.responses < 5 &&
+                    this.state.questionBank.map(({question, answers, correct, questionId}) => (
+                        <QuestionBox 
+                        question={question} 
+                        options={answers} 
+                        key={questionId}
+                        selected={answer => this.computeAnswer(answer, correct)}
+                        />
+                    ))
+                }
+                {this.state.responses === 5 ?  (
+                    <Result score={this.state.score} playAgain={this.playAgain} />
+                ) : null}
             </div>
         );
     }
